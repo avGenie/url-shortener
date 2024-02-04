3d7d2ecbc9e5c7781c7c7ae675a1d7a214fe2706
avGenie <aleksandr-generalov19@yandex.ru>
2024-02-04 12:45:10 +0300

    added tests for GetHandler

--- internal/app/handlers/handlers_test.go
+++ internal/app/handlers/handlers_test.go
@@ -82,3 +82,89 @@ func TestPostHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestGetHandler(t *testing.T) {
+	type want struct {
+		statusCode  int
+		contentType string
+		location    string
+		message     string
+	}
+	tests := []struct {
+		name    string
+		request string
+		urls    map[string]string
+		want    want
+	}{
+		{
+			name:    "correct input data",
+			request: "/aHR0cHM6",
+
+			urls: map[string]string{
+				"aHR0cHM6": "https://practicum.yandex.ru/",
+			},
+
+			want: want{
+				statusCode:  http.StatusTemporaryRedirect,
+				contentType: "text/plain",
+				location:    "https://practicum.yandex.ru/",
+				message:     "",
+			},
+		},
+		{
+			name:    "request without id",
+			request: "/",
+
+			urls: map[string]string{
+				"aHR0cHM6": "https://practicum.yandex.ru/",
+			},
+
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain",
+				location:    "",
+				message:     ErrInvalidGivenURL.Error(),
+			},
+		},
+		{
+			name:    "missing URL",
+			request: "/fsdfuytu",
+
+			urls: map[string]string{
+				"aHR0cHM6": "https://practicum.yandex.ru/",
+			},
+
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain",
+				location:    "",
+				message:     ShortUrlNotInDb,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			urls = test.urls
+
+			request := httptest.NewRequest(http.MethodGet, test.request, nil)
+			writer := httptest.NewRecorder()
+
+			GetHandler(writer, request)
+
+			res := writer.Result()
+
+			assert.Equal(t, test.want.statusCode, res.StatusCode)
+			assert.Equal(t, test.want.contentType, res.Header.Get("Content-Type"))
+			assert.Equal(t, test.want.location, res.Header.Get("Location"))
+
+			userResult, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+
+			err = res.Body.Close()
+			require.NoError(t, err)
+
+			assert.Equal(t, test.want.message, string(userResult))
+		})
+	}
+}
