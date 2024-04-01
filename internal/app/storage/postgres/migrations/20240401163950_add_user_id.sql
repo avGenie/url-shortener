85ea2eb6ef8c86db36781fecc785804647643c76
avGenie <aleksandr-generalov19@yandex.ru>
2024-04-01 20:31:58 +0300

    changed postgres architecture

---
+++ internal/app/storage/postgres/migrations/20240401163950_add_user_id.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE url DROP CONSTRAINT url_short_url_key;
+ALTER TABLE url ADD user_id uuid;
+ALTER TABLE url ALTER COLUMN user_id SET NOT NULL;
+ALTER TABLE url DROP CONSTRAINT url_pkey;
+ALTER TABLE url ADD CONSTRAINT url_pkey PRIMARY KEY (user_id, short_url);
+
+CREATE TABLE IF NOT EXISTS users(
+    id uuid PRIMARY KEY
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+CREATE INDEX IF NOT EXISTS idx_short_url ON url(short_url);
+ALTER TABLE url DROP COLUMN user_id;
+ALTER TABLE url DROP CONSTRAINT url_pkey;
+ALTER TABLE url ADD PRIMARY KEY (id);
+
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
