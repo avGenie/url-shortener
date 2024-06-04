792fcb8ed1b6c0a1ed4fdb717f6d857788a70648
avGenie <aleksandr-generalov19@yandex.ru>
2024-06-04 10:17:03 +0300

    added another statickcheck analyzers

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/sethvargo/go-retry v0.2.4 // indirect
--- internal/app/usecase/staticlint/staticcheck/staticcheck.go
+++ internal/app/usecase/staticlint/staticcheck/staticcheck.go
@@ -3,16 +3,48 @@ package staticcheck
 
 import (
 	"golang.org/x/tools/go/analysis"
+	"honnef.co/go/tools/quickfix"
+	"honnef.co/go/tools/simple"
 	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
+)
+
+var (
+	simpleAnalyzers = map[string]struct{}{
+		"S1011": struct{}{},
+		"S1017": struct{}{},
+	}
+	stylecheckAnalyzers = map[string]struct{}{"ST1023": struct{}{}}
+	quickfixAnalyzers   = map[string]struct{}{"QF1007": struct{}{}}
 )
 
 // GetAnalyzers Returns all SA staticcheck analyzers
 //
 // List of analyzers: https://staticcheck.io/docs/checks
 func GetAnalyzers() []*analysis.Analyzer {
-	analyzers := make([]*analysis.Analyzer, 0, len(staticcheck.Analyzers))
+	count := len(staticcheck.Analyzers) + len(simpleAnalyzers) +
+		len(stylecheckAnalyzers) + len(quickfixAnalyzers)
+	analyzers := make([]*analysis.Analyzer, 0, count)
 	for _, v := range staticcheck.Analyzers {
 		analyzers = append(analyzers, v.Analyzer)
+	}
+
+	for _, v := range simple.Analyzers {
+		if _, ok := simpleAnalyzers[v.Analyzer.Name]; ok {
+			analyzers = append(analyzers, v.Analyzer)
+		}
+	}
+
+	for _, v := range stylecheck.Analyzers {
+		if _, ok := stylecheckAnalyzers[v.Analyzer.Name]; ok {
+			analyzers = append(analyzers, v.Analyzer)
+		}
+	}
+
+	for _, v := range quickfix.Analyzers {
+		if _, ok := quickfixAnalyzers[v.Analyzer.Name]; ok {
+			analyzers = append(analyzers, v.Analyzer)
+		}
 	}
 
 	return analyzers
